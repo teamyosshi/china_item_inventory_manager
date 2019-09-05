51dc37d38893d3b22d4803c1e18e8a68193eafdc
ryoji125 <nyarun115@gmail.com>
2019-09-06 03:58:26 +0900

    #2 ヘッダーボタンの作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'users/:id/Inventory_control', to:'stocks#Inventory_control', as: :inventory_control
   get 'users/:id/item_research', to:'researches#item_research', as: :item_research
   get 'users/:id/product_inventory', to:'stocks#product_inventory', as: :stock
-  get'users/:id/reseach', to:'reseachs#stocking', as: :stocking
+  get'users/:id/research', to:'researches#stocking', as: :stocking
   get 'users/:id/product_scarce', to:'items#product_scarce', as: :product_scarce
   get 'users/:id/sold_out', to: 'stocks#sold_out', as: :sold_out
   get 'users', to: 'users#index', as: :index
