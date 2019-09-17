d730affa023127fa01f4e87272b70882b931414e
Hayato Saito <omimisama@gmail.com>
2019-09-17 17:57:54 +0900

    #1_add_research_page_layout

--- app/controllers/researches_controller.rb
+++ app/controllers/researches_controller.rb
@@ -61,6 +61,12 @@ class ResearchesController < ApplicationController
     end
   end
 
+  def item_research
+    # @item = Item.all
+  end
+
+
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_research
