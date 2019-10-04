549bdce535c2f259c445d3c26d6041044d0f5f8f
Your Name <your.email@example.com>
2019-10-04 01:44:56 +0000

    chana-import3-3-1

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
     has_many :stocks, dependent: :destroy
+ 
+    def self.search(search) #self.でクラスメソッドとしている
+        if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
+            #Item.where(['item_number LIKE ?', "%#{search}%"])
+            Item.where('item_number LIKE ? OR part_number LIKE ?', "%#{search}%", "%#{search}%")
+        #else
+            #Item.all #全て表示。
+        end
+    end
 end
