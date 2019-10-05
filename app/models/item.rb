5ab47e199c17026376da4b6295d4aa800241e403
Your Name <your.email@example.com>
2019-10-05 10:46:23 +0000

    china-import-3.3.8

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
     has_many :stocks, dependent: :destroy
+
     def self.search(search) #self.でクラスメソッドとしている
       if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
         Item.where('item_number LIKE ? OR part_number LIKE ?', "%#{search}%", "%#{search}%")
