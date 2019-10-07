1f646f67fd974286f83d397e21a2e185924ed8a3
Hayato Saito <omimisama@gmail.com>
2019-10-07 21:36:34 +0900

    #43-modify_conflict_research_btn

--- app/controllers/researches_controller.rb
+++ app/controllers/researches_controller.rb
@@ -1,6 +1,9 @@
 class ResearchesController < ApplicationController
   before_action :set_research, only: [:show, :edit, :update, :destroy]
 
+  require 'open-uri'
+  require 'nokogiri'
+
   # GET /researches
   # GET /researches.json
   def index
@@ -61,17 +64,46 @@ class ResearchesController < ApplicationController
     end
   end
 
+  # リサーチページ表示
   def item_research
-    @researches = Research.paginate(page: params[:page], per_page: 50)
+    @researches = Research.paginate(page: params[:page], per_page: 50).order(created_at: "ASC")
+    @research = Research.all
+  end
+
+  # スクレイピング
+  def item_research_scrape
+    @uri = "https://auctions.yahoo.co.jp/search/search?p=#{params[:search]}"
+    @doc = Nokogiri::HTML(open(@uri),nil,"utf-8")
+    charset = nil
+    html = open(@uri) do |f|
+      charset = f.charset # 文字種別を取得
+      f.read # htmlを読み込んで変数htmlに渡す
+    end
+    # htmlをパース(解析)してオブジェクトを作成
+    @doc = Nokogiri::HTML.parse(html, nil, charset)
+    # 商品の各ページタイトル
+    # @doc.xpath("//div[@class='Products Products--grid']/ul[@class='Products__items']/li[@class='Product'][1]/div[@class='Product__detail']/h3[@class='Product__title']/a[@class='Product__titleLink']").each do |node|
+    #   node.text
+    # Research.create(japan_title: node, user_id: current_user)
+    # end
+    # 画像
+    # debugger
+    @doc.each do |photo|
+      if photo.include?("#{params[:search]}")
+        Research.create(japan_img: photo, user_id: current_user, item_id: 1)
+      end
+    end
+
+    redirect_to item_research_path
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_research
-      @research = Research.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_research
+    @research = Research.find(params[:id])
+  end
 
-    def research_params
-      params.require(:research).permit(:japan_image_url, :japan_title, :japan_url, :japan_description, :japan_price, :china_image_url, :china_title, :china_url, :china_price, :item_id, :user_id)
-    end
+  def research_params
+    params.require(:research).permit(:japan_image_url, :japan_title, :japan_url, :japan_description, :japan_price, :china_image_url, :china_title, :china_url, :china_price, :item_id, :user_id)
+  end
 end
