68d3b7067be81167247cde3d8ec05f2a91c8433f
ryoji125 <nyarun115@gmail.com>
2019-10-22 14:20:17 +0900

    #49 リダイレクト処理先が間違っていたので修正

--- app/controllers/take_inventory_controller.rb
+++ app/controllers/take_inventory_controller.rb
@@ -4,11 +4,10 @@ class TakeInventoryController < ApplicationController
     @takeinventory = TakeInventory.find_by(fiscal_year: now_year)
     if @takeinventory.update(takeinventory_params)
       flash[:success] = "機種商品棚卸高を登録しました。"
-      redirect_to root_path
     else
       flash[:danger] = "機種商品棚卸高に失敗しました。再度やり直してください。"
-      redirect_to root_path
     end
+    redirect_to stock_path(current_user)
   end
 private
   def takeinventory_params
