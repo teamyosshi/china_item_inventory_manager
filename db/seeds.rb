93a7b863431425136136bb952c4db576f9216535
T.Ito <you@example.com>
2019-12-31 08:14:24 +0000

    #154商品新規登録に関する修正等

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,6 @@ User.create!(name: "Sample User",
              password_confirmation: "password",
              address: "神奈川県横浜市戸塚区俣野町1403",
              phone_number: "080-1234-1234",
+             inventory_manager_flg: true,
+             reserch_user_flg: true,
              admin: 1)
-10.times do |n|
-  name = Faker::Name.name
-  email = "test-#{n+1}@email.com"
-  password = "password"
-  address = "神奈川県横浜市戸塚区俣野町#{n+1}"
-  phone_number = "080-1234-1234#{n+1}"
-  User.create!(name: name,
-               line_id: "123456789",
-               email: email,
-               password: password,
-               password_confirmation: password,
-               address: address,
-               phone_number: phone_number)
-end
