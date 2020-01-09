b7760123b241f6c47adb01c8c1a7af09b18b9b80
Tomohiro Hagino <marspeoplehg@gmail.com>
2020-01-09 18:49:34 +0900

    なぜかフィルタリングされてしまうのを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:show, :edit, :update]
   before_action :correct_user, only: [:edit, :update,:show]
   before_action :admin_user,　only: [:index, :destroy]
+  skip_before_action  :admin_user,　only: :edit
 
   # GET /users
   # GET /users.json
