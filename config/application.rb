28ba36367d9cdde64ac7f858e61474712f4117ed
Tomohiro Hagino <marspeoplehg@gmail.com>
2020-03-10 22:28:44 +0900

    testディレクトリ作らないように追記

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 
 module ChinaInventory
   class Application < Rails::Application
+    config.generators.system_tests = nil
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.time_zone = 'Asia/Tokyo'
